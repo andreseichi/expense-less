6d6a22c817e20f950732156c01cf76c47626c052
andreseichi <andre_seichi@hotmail.com>
2022-10-02 18:09:34 -0300

    feat: sidebar

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,18 @@
+import { Flex, Text } from "@chakra-ui/react";
+
+import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
+
+export default function Dashboard() {
+  return (
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" my="6" maxWidth="1480" mx="auto" px="6">
+        <Sidebar />
+
+        <Text>Dashboard</Text>
+      </Flex>
+    </Flex>
+  );
+}
