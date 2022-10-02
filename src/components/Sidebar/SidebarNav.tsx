4a2598de0ac67a648d842fa8d142e5947fb56b8f
andreseichi <andre_seichi@hotmail.com>
2022-10-02 18:08:47 -0300

    feat: sidebar component

---
+++ src/components/Sidebar/SidebarNav.tsx
@@ -0,0 +1,25 @@
+import { Stack } from "@chakra-ui/react";
+import {
+  RiBarChartFill,
+  RiDashboardLine,
+  RiGitMergeLine,
+  RiInputMethodLine,
+} from "react-icons/ri";
+
+import { NavLink } from "./NavLink";
+import { NavSection } from "./NavSection";
+
+export function SidebarNav() {
+  return (
+    <Stack spacing="12" align="flex-start">
+      <NavSection title="GERAL">
+        <NavLink href="/dashboard" icon={RiDashboardLine}>
+          Dashboard
+        </NavLink>
+        <NavLink href="/graphics" icon={RiBarChartFill}>
+          Graphics
+        </NavLink>
+      </NavSection>
+    </Stack>
+  );
+}
