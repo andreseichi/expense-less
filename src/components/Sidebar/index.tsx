00946d376e5b974fd4624e6eac9abefd621f2f1d
andreseichi <andre_seichi@hotmail.com>
2022-10-02 18:13:07 -0300

    fix: portuguese text

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -27,7 +27,7 @@ export function Sidebar() {
           <DrawerContent bg="gray.800" p="4">
             <DrawerCloseButton mt="6" />
 
-            <DrawerHeader>Navegação</DrawerHeader>
+            <DrawerHeader>Navigation</DrawerHeader>
 
             <DrawerBody>
               <SidebarNav />
