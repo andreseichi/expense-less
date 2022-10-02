3b396b2d86f507d5eb5969be6023cc5120603280
andreseichi <andre_seichi@hotmail.com>
2022-10-02 18:06:54 -0300

    feat: using user info from context

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,26 +1,26 @@
-import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
+import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 interface ProfileProps {
   showProfileData: boolean;
 }
 
 export function Profile({ showProfileData }: ProfileProps) {
+  const { user } = useContext(AuthContext);
+
   return (
     <Flex align="center">
       {showProfileData && (
         <Box mr="4" textAlign="right">
-          <Text>André Tashiro</Text>
+          <Text>{user.name}</Text>
           <Text color="gray.300" fontSize="small">
-            andre_seichi@hotmail.com
+            {user.email}
           </Text>
         </Box>
       )}
 
-      <Avatar
-        size="md"
-        name="André Tashiro"
-        src="https://github.com/andreseichi.png"
-      />
+      <Avatar size="md" name={user.name} src={user.pictureUrl} />
     </Flex>
   );
 }
