c0bc2fa930a05f98bec6815ac3534574e03c4760
andreseichi <andre_seichi@hotmail.com>
2022-10-02 18:08:08 -0300

    feat: sidebar provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,15 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme";
 import { AuthProvider } from "../context/AuthContext";
+import { SidebarDrawerProvider } from "../context/SidebarDrawerContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <AuthProvider>
-        <Component {...pageProps} />
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
       </AuthProvider>
     </ChakraProvider>
   );
