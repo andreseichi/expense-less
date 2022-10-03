1d429c8a736caacfd2afff2f6d7030efd687d35d
andreseichi <andre_seichi@hotmail.com>
2022-10-02 21:46:21 -0300

    feat: query client provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,27 @@
 import type { AppProps } from "next/app";
 
 import { ChakraProvider } from "@chakra-ui/react";
-import { theme } from "../styles/theme";
 import { AuthProvider } from "../context/AuthContext";
 import { SidebarDrawerProvider } from "../context/SidebarDrawerContext";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import { theme } from "../styles/theme";
+import { queryClient } from "../services/queryClient";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <AuthProvider>
-        <SidebarDrawerProvider>
-          <Component {...pageProps} />
-        </SidebarDrawerProvider>
-      </AuthProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <AuthProvider>
+          <SidebarDrawerProvider>
+            <Component {...pageProps} />
+          </SidebarDrawerProvider>
+        </AuthProvider>
+      </ChakraProvider>
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
