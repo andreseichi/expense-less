59016baa56c275eeb877d112d3b908dab25d7366
andreseichi <andre_seichi@hotmail.com>
2022-10-03 23:28:22 -0300

    feat: signout if expired token

--- src/services/hooks/useCategories.ts
+++ src/services/hooks/useCategories.ts
@@ -3,6 +3,7 @@ import {
   UseQueryOptions,
   UseQueryResult,
 } from "@tanstack/react-query";
+import { signOut } from "../../context/AuthContext";
 import { api } from "../api";
 
 type Category = {
@@ -14,16 +15,27 @@ type getCategoriesResponse = {
   categories: Category[];
 };
 
-export async function getCategories(): Promise<getCategoriesResponse> {
+export async function getCategories(): Promise<
+  getCategoriesResponse | unknown
+> {
   const token = window.localStorage.getItem("@Expenseless:token");
   const config = {
     headers: {
       Authorization: `Bearer "${token}"`,
     },
   };
-  const { data } = await api.get("category", config);
+  try {
+    const response = await api.get("category", config);
+    const { data } = response;
 
-  return { categories: data };
+    return { categories: data };
+  } catch (error: any) {
+    if (error.response.status === 401) {
+      window.localStorage.removeItem("@Expenseless:token");
+
+      signOut();
+    }
+  }
 }
 
 export function useCategories(options?: UseQueryOptions) {
