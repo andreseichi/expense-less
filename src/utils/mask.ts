020b009d0c02656be5792befb2f8ecf97d3dc9d2
andreseichi <andre_seichi@hotmail.com>
2022-10-08 19:48:51 -0300

    feat: mask for currency input

---
+++ src/utils/mask.ts
@@ -0,0 +1,12 @@
+import { FormEvent } from "react";
+
+export function currency(e: FormEvent<HTMLInputElement>) {
+  let value = e.currentTarget.value;
+  value = value.replace(/\D/g, "");
+  value = value.replace(/(\d)(\d{2})$/, "$1,$2");
+  value = value.replace(/(?=(\d{3})+(\D))\B/g, ".");
+
+  e.currentTarget.value = value;
+
+  return e;
+}
