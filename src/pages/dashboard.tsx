c9eabf444e4ab09d7b4fa15cf8e8d5fe2d69b171
andreseichi <andre_seichi@hotmail.com>
2022-10-08 18:29:15 -0300

    feat: added modal for new transaction

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -2,26 +2,80 @@ import {
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbLink,
+  Button,
   Flex,
   Icon,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Select,
   SimpleGrid,
+  Stack,
   Text,
+  useDisclosure,
 } from "@chakra-ui/react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useRef } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import {
   RiAddFill,
   RiMoneyDollarCircleLine,
   RiShoppingCartLine,
   RiWallet3Line,
 } from "react-icons/ri";
-import MiniStatistics from "../components/Dashboard/MiniStatistics";
+import * as yup from "yup";
 
+import MiniStatistics from "../components/Dashboard/MiniStatistics";
+import { Input } from "../components/Form/Input";
+import { Switch } from "../components/Form/Switch";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
 import { Transactions } from "../components/Transactions";
+import { useCategories } from "../services/hooks/useCategories";
 import { useTransactions } from "../services/hooks/useTransactions";
+
+type newTransactionFormData = {
+  name: string;
+  description: string;
+  amount: number;
+  type: boolean;
+  categoryId: number;
+};
+
+const newTransactionFormSchema = yup.object().shape({
+  type: yup.string().required("Type required"),
+  amount: yup.string().typeError("Value is required"),
+  description: yup.string().required("Description required"),
+  name: yup.string().required("Name of transaction required"),
+  categoryId: yup.number().required("Category required"),
+});
 
 export default function Dashboard() {
   const { data } = useTransactions();
+  const { data: categories } = useCategories();
+
+  const initialRefModal = useRef(null);
+  const finalRefModal = useRef(null);
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const { register, handleSubmit, formState } = useForm<newTransactionFormData>(
+    {
+      resolver: yupResolver(newTransactionFormSchema),
+    }
+  );
+  const errors = formState.errors;
+
+  const handleNewTransaction: SubmitHandler<newTransactionFormData> = (
+    values
+  ) => {
+    // signIn(values);
+    console.log("values", values);
+  };
 
   return (
     <Flex direction="column" h="100vh">
@@ -51,6 +105,7 @@ export default function Dashboard() {
               ml="2"
               bg="pink.500"
               rounded="full"
+              onClick={onOpen}
             />
           </Flex>
 
@@ -80,6 +135,87 @@ export default function Dashboard() {
           <Transactions />
         </Flex>
       </Flex>
+
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        blockScrollOnMount
+        initialFocusRef={initialRefModal}
+        finalFocusRef={finalRefModal}
+      >
+        <ModalOverlay />
+        <ModalContent bg="green.500">
+          <ModalHeader>Register new transaction</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <Flex
+              as="form"
+              w="100%"
+              flexDir="column"
+              onSubmit={handleSubmit(handleNewTransaction)}
+            >
+              <Stack spacing="4">
+                <Input
+                  {...register("name")}
+                  name="name"
+                  label="Name"
+                  type="text"
+                  error={errors.name}
+                />
+                <Input
+                  {...register("description")}
+                  name="description"
+                  label="Description"
+                  type="text"
+                  error={errors.description}
+                />
+                <Input
+                  {...register("amount")}
+                  name="amount"
+                  label="Value $"
+                  type="text"
+                  error={errors.amount}
+                />
+
+                <Switch
+                  {...register("type")}
+                  name="type"
+                  label="Expense"
+                  error={errors.type}
+                />
+
+                <Select
+                  {...register("categoryId")}
+                  bgColor="gray.900"
+                  variant="filled"
+                  _hover={{
+                    bgColor: "gray.900",
+                  }}
+                  size="lg"
+                >
+                  {categories?.categories.map((option) => (
+                    <option key={option.id} value={option.id}>
+                      {option.name}
+                    </option>
+                  ))}
+                </Select>
+              </Stack>
+
+              <Button
+                type="submit"
+                mt="6"
+                colorScheme="pink"
+                size="lg"
+                isLoading={formState.isSubmitting}
+              >
+                Create
+              </Button>
+            </Flex>
+          </ModalBody>
+
+          <ModalFooter></ModalFooter>
+        </ModalContent>
+      </Modal>
     </Flex>
   );
 }
