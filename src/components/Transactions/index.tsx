9c808de4ad4a93679b53ee3d99837da88773f3aa
andreseichi <andre_seichi@hotmail.com>
2022-10-09 15:45:52 -0300

    feat: delete transaction with popover

--- src/components/Transactions/index.tsx
+++ src/components/Transactions/index.tsx
@@ -1,12 +1,67 @@
-import { Box, Flex, Icon, Text, Tooltip } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Icon,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverFooter,
+  PopoverHeader,
+  PopoverTrigger,
+  Text,
+  Tooltip,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
+import { useMutation } from "@tanstack/react-query";
+import { RiCheckLine, RiDeleteBinLine } from "react-icons/ri";
 
-import { useTransactions } from "../../services/hooks/useTransactions";
+import {
+  deleteTransaction,
+  useTransactions,
+} from "../../services/hooks/useTransactions";
+import { queryClient } from "../../services/queryClient";
 import { animationGridItem, MotionFlex } from "../../styles/animations";
 
 import { categoryIcon } from "../../utils/categoryIcon";
 
 export function Transactions() {
+  const toast = useToast();
+  const { onClose } = useDisclosure();
+
   const { data } = useTransactions();
+
+  const deleteTransactionMutation = useMutation(deleteTransaction, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(["transactions"]);
+    },
+  });
+
+  async function handleDeleteTransaction(id: number) {
+    const response = await deleteTransactionMutation.mutateAsync(id);
+
+    if (response?.status === 204) {
+      toast({
+        title: `Transaction ${response.data} deleted`,
+        description: "Your transaction has been deleted",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      });
+    } else {
+      toast({
+        title: `Error ${response?.status}`,
+        description: `${response?.data.map((error: string) => error)}`,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+    onClose();
+  }
 
   return (
     <Box w="100%">
@@ -17,13 +72,12 @@ export function Transactions() {
             justify="space-between"
             w="100%"
             align="center"
-            _hover={{ cursor: "pointer" }}
             variants={animationGridItem}
             initial="hidden"
             animate="visible"
             custom={index - 0.5}
           >
-            <Flex align="center">
+            <Flex align="center" _hover={{ cursor: "pointer" }}>
               <Icon
                 as={categoryIcon[transaction.Category.name]}
                 fontSize="35"
@@ -40,13 +94,60 @@ export function Transactions() {
               </Tooltip>
             </Flex>
 
-            <Text
-              fontSize="2xl"
-              fontWeight="bold"
-              color={transaction.type === "income" ? "#50DA7E" : "#E3170A"}
-            >
-              {transaction.amount}
-            </Text>
+            <Flex align="center" justifyContent="end" gap={6}>
+              <Text
+                fontSize="2xl"
+                fontWeight="bold"
+                color={transaction.type === "income" ? "#50DA7E" : "#E3170A"}
+              >
+                {transaction.amount}
+              </Text>
+
+              <Popover>
+                <PopoverTrigger>
+                  <Button
+                    aria-label="Delete transaction"
+                    bg="pink.500"
+                    _hover={{ background: "pink.600" }}
+                    _active={{ background: "pink.700" }}
+                  >
+                    <Icon as={RiDeleteBinLine} fontSize="20" />
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent
+                  bg="teal.300"
+                  color="gray.800"
+                  borderColor="teal.800"
+                >
+                  <PopoverArrow bg="teal.500" />
+                  <PopoverCloseButton />
+                  <PopoverHeader borderColor="teal.800">
+                    Delete transaction {transaction.name}?
+                  </PopoverHeader>
+                  <PopoverBody>
+                    Are you sure you want to delete the {transaction.name}{" "}
+                    transaction?
+                  </PopoverBody>
+                  <PopoverFooter
+                    borderColor="teal.800"
+                    display="flex"
+                    justifyContent="flex-end"
+                  >
+                    <Button
+                      leftIcon={<RiCheckLine />}
+                      size="sm"
+                      bg="pink.500"
+                      _hover={{ background: "pink.600" }}
+                      _active={{ background: "pink.700" }}
+                      color="white"
+                      onClick={() => handleDeleteTransaction(transaction.id)}
+                    >
+                      Confirm
+                    </Button>
+                  </PopoverFooter>
+                </PopoverContent>
+              </Popover>
+            </Flex>
           </MotionFlex>
         ))}
       </Flex>
